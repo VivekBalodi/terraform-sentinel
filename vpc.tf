8857101e132f23a26ae5b9cd77810ad8df2548e7
VivekBalodi <61856789+VivekBalodi@users.noreply.github.com>
2021-08-02 11:39:46 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -5,36 +5,36 @@ resource "google_compute_network" "vpc" {
  auto_create_subnetworks = "false"
 }
 
-// Create Subnet
-resource "google_compute_subnetwork" "subnet" {
- name          = var.subnet_name
- ip_cidr_range = var.subnet_cidr
- network       = google_compute_network.vpc.id
- depends_on    = ["google_compute_network.vpc"]
- region      = var.region
-}
+# // Create Subnet
+# resource "google_compute_subnetwork" "subnet" {
+#  name          = var.subnet_name
+#  ip_cidr_range = var.subnet_cidr
+#  network       = google_compute_network.vpc.id
+#  depends_on    = ["google_compute_network.vpc"]
+#  region      = var.region
+# }
 
-//google cloud instance
+# //google cloud instance
 
-resource "google_compute_address" "static" {
-  name = "ipv4-address1"
-}
+# resource "google_compute_address" "static" {
+#   name = "ipv4-address1"
+# }
 
-resource "google_compute_instance" "restrict-public-ip-vm" {
-  description  = "Testing the Sentinel policy to check if Public IP is disabled"
-  name         = var.vm_name
-  machine_type = var.machine_size
-  zone         = var.zone
-  project      = var.project
-  depends_on    = ["google_compute_network.vpc"]
+# resource "google_compute_instance" "restrict-public-ip-vm" {
+#   description  = "Testing the Sentinel policy to check if Public IP is disabled"
+#   name         = var.vm_name
+#   machine_type = var.machine_size
+#   zone         = var.zone
+#   project      = var.project
+#   depends_on    = ["google_compute_network.vpc"]
 
-  boot_disk {
-    initialize_params {
-      image = var.image_name
-    }
-  }
+#   boot_disk {
+#     initialize_params {
+#       image = var.image_name
+#     }
+#   }
 
-  network_interface {
-    subnetwork = google_compute_subnetwork.subnet.self_link
-  }
-}
+#   network_interface {
+#     subnetwork = google_compute_subnetwork.subnet.self_link
+#   }
+# }
